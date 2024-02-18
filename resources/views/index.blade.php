3835d133cf09c6055f0ab9b32053a95849f32c71
Patrick Gaines <patrickagaines@outlook.com>
2024-02-18 13:21:55 -0600

    Install local variable font file, open sans. Create separate tailwind css file for public facing site

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,7 @@
 <x-site-layout>
+    <div class="max-w-md">
     <h1>Hello Layout</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias amet, animi architecto deleniti dignissimos distinctio error explicabo fugit impedit labore libero necessitatibus, nesciunt non obcaecati tenetur, unde vel!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dignissimos ea iusto libero neque quibusdam, veritatis. Blanditiis, natus, tempora. Architecto est ex nesciunt odit quaerat quia. Animi asperiores atque blanditiis consequatur est incidunt labore natus quas quos ratione tempore, tenetur ullam. Debitis dolore est hic nam, omnis possimus provident quis.</p>
+    </div>
 </x-site-layout>
