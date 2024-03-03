7ff6309903f9792a6065dbf10f766634f9c3bf7a
Patrick Gaines <patrickagaines@outlook.com>
2024-03-03 15:28:02 -0600

    Move Post Controller into dashboard subdirectory

--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\dashboard;
 
+use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
