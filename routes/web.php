818f4d30374bff51b4ed3c9e626c4acead14bdce
Patrick Gaines <patrickagaines@outlook.com>
2024-03-05 19:47:14 -0600

    Update Http/Controllers/Dashboard directory to match psr-4 standards

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\dashboard\PostController;
+use App\Http\Controllers\Dashboard\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
