b11fd276f11031ddc850b5c67d2410710b37ec9d
Patrick Gaines <patrickagaines@outlook.com>
2024-03-05 20:03:22 -0600

    Add required attribute to title input for client side validation. Will prevent losing image uploads when performing this check on the server side

--- resources/views/components/dashboard/posts/form.blade.php
+++ resources/views/components/dashboard/posts/form.blade.php
@@ -10,8 +10,8 @@
         </x-primary-button>
     </div>
     <div class="mb-6">
-        <x-input-label for="title">Title</x-input-label>
-        <x-text-input id="title" name="title" class="w-full" />
+        <x-input-label for="title">Title*</x-input-label>
+        <x-text-input id="title" name="title" class="w-full" required />
     </div>
     <div class="flex justify-center mb-6">
         <label for="image_upload" class="text-gray-700 cursor-pointer rounded-md border border-gray-300 shadow-sm bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest transition duration-150 ease-in-out hover:bg-gray-50">Upload Images</label>
