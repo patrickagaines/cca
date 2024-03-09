682fd9ab6c297ab19495fbb7281a1b591537ca40
Patrick Gaines <patrickagaines@outlook.com>
2024-03-09 14:09:45 -0600

    Make posts edit view header dynamic. Add edit and delete buttons. Modify built in button styles

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -1,12 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Posts') }}
+            {{ __($post->title) }}
         </h2>
     </x-slot>
 
     <div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
-        {{ $post->title }}
+        <div class="mb-4 flex justify-end gap-2">
+            <x-dashboard.link-button :href="route('dashboard.posts.edit', $post)">
+                Edit
+            </x-dashboard.link-button>
+            <x-danger-button>
+                Delete
+            </x-danger-button>
+        </div>
     </div>
 
 </x-app-layout>
