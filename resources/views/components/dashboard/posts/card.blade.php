910579da4a0ea838c90706f1959332bc60f6abb7
Patrick Gaines <patrickagaines@outlook.com>
2024-03-10 16:35:01 -0500

    Change dashboard post card thumbnail to square aspect ratio

--- resources/views/components/dashboard/posts/card.blade.php
+++ resources/views/components/dashboard/posts/card.blade.php
@@ -2,7 +2,7 @@
 
 <div class="rounded-sm bg-white p-4 shadow">
     <div class="mb-4">
-        <img src="{{ asset($post->images()->first()->file_path) }}" alt="">
+        <img alt="" src="{{ asset($post->images()->first()->file_path) }}" class="object-cover aspect-square">
     </div>
     <div class="mb-4 h-16">
         <h3>{{ $post->title }}</h3>
