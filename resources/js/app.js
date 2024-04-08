caefc5e4782ade118c4948d764d381378da888f4
Patrick Gaines <patrickagaines@outlook.com>
2024-04-08 10:35:47 -0500

    Move javascript for opening and closing delete confirmation window dialogue to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import './bootstrap';
 
 import Alpine from 'alpinejs';
@@ -17,4 +19,18 @@ if (flashMessage) {
         flashMessage.classList.remove('flash_in');
         flashMessage.classList.add('flash_out');
     }, 3000);
+}
+
+const deleteDialogue = document.querySelector('.window_dialogue.confirm_delete');
+const deleteButton = document.querySelector('button.delete');
+const cancelDeleteButton = document.querySelector('button.cancel_delete');
+
+if (deleteDialogue && deleteButton && cancelDeleteButton) {
+    deleteButton.addEventListener('click', function() {
+        deleteDialogue.classList.add('window_dialogue--active');
+    });
+
+    cancelDeleteButton.addEventListener('click', function() {
+        deleteDialogue.classList.remove('window_dialogue--active');
+    });
 }
