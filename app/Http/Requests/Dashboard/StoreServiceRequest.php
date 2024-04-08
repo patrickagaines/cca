68ab6e590055d73048392519a0638f2fab0830d6
Patrick Gaines <patrickagaines@outlook.com>
2024-04-08 10:41:49 -0500

    Create store and update form requests for services

---
+++ app/Http/Requests/Dashboard/StoreServiceRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Dashboard;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreServiceRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:60',
+            'description' => 'required|max:1500'
+        ];
+    }
+}
