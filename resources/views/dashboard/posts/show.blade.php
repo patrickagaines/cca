0a2bceacc1fb4c9a291e8d846bae859ce422489d
Patrick Gaines <patrickagaines@outlook.com>
2024-04-08 10:41:13 -0500

    Modify posts show blade to use posts delete component

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -1,6 +1,4 @@
 <x-app-layout>
-    <x-slot name="scripts">@vite(['resources/js/dashboard/posts/show.js'])</x-slot>
-
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __($post->title) }}
@@ -18,24 +16,8 @@
         </div>
     </div>
 
-    <x-dashboard.window-dialogue>
-        <form action="{{ route('dashboard.posts.destroy', $post) }}"
-              method="POST"
-        >
-            @csrf
-            @method('DELETE')
-            <h3 class="text-lg">Are you sure you wish to delete
-                <span class="font-semibold"> {{ $post->title }}</span>?
-            </h3>
-            <div class="mt-8 flex justify-end gap-2">
-                <x-secondary-button type="button" class="cancel_delete">
-                    Cancel
-                </x-secondary-button>
-                <x-danger-button>
-                    Delete
-                </x-danger-button>
-            </div>
-        </form>
+    <x-dashboard.window-dialogue class="confirm_delete">
+        <x-dashboard.posts.delete :post="$post"/>
     </x-dashboard.window-dialogue>
 </x-app-layout>
 
