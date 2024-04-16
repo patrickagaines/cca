0c8ff4baccb8976a95f155ba5c207225dcc72209
Patrick Gaines <patrickagaines@outlook.com>
2024-04-16 11:18:19 -0500

    Modify site header to accomodate maximum page width of 1440px.

--- resources/views/components/site/layout/header.blade.php
+++ resources/views/components/site/layout/header.blade.php
@@ -1,4 +1,6 @@
-<header class="fixed top-0 right-0 left-0 flex items-center justify-between p-4 h-[60px] bg-ground bg-opacity-90">
-    <x-site.svgs.cca-logo-short class="h-[32px]"/>
-    <x-site.navigation.mobile-menu/>
+<header class="fixed top-0 right-0 left-0 bg-opacity-90 bg-ground">
+    <div class="mx-auto flex items-center justify-between p-4 h-[60px] max-w-[1440px] lg:h-[80px]">
+        <x-site.svgs.cca-logo-short class="h-[32px]"/>
+        <x-site.navigation.mobile-menu/>
+    </div>
 </header>
