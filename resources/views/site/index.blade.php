37d7aa1350b3d5902ea36a93f68a057b34d462a9
Patrick Gaines <patrickagaines@outlook.com>
2024-04-16 11:34:10 -0500

    Add hero image to homepage

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,7 +1,7 @@
 <x-site-layout>
-    <div class="max-w-md">
-    <h1>Hello Layout</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias amet, animi architecto deleniti dignissimos distinctio error explicabo fugit impedit labore libero necessitatibus, nesciunt non obcaecati tenetur, unde vel!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dignissimos ea iusto libero neque quibusdam, veritatis. Blanditiis, natus, tempora. Architecto est ex nesciunt odit quaerat quia. Animi asperiores atque blanditiis consequatur est incidunt labore natus quas quos ratione tempore, tenetur ullam. Debitis dolore est hic nam, omnis possimus provident quis.</p>
+    <x-site.svgs.cca-sketch-mobile class="lg:hidden"/>
+    <x-site.svgs.cca-sketch-desktop class="hidden lg:block"/>
+    <div class="p-4 bg-surface">
+        <h1 class="text-center text-2xl font-yellowtail">Quality Service Since 1995</h1>
     </div>
 </x-site-layout>
